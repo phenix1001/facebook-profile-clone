6e699cae095d5518cf236af9adc1041c1f74c085
A'z O'ssama <88379336+phenix1001@users.noreply.github.com>
2023-07-09 21:34:07 +0100

    Add files via upload

---
+++ main.js
@@ -0,0 +1,13 @@
+const fileUploadInput = document.getElementById('file-upload');
+const profilePicture = document.getElementById('profile-picture');
+
+fileUploadInput.addEventListener('change', function (event) {
+    const file = event.target.files[0];
+    if (file) {
+        const reader = new FileReader();
+
+        reader.addEventListener('load', function () {
+            profilePicture.setAttribute('src', reader.result);
+        });
+    }
+});
